a356ac37b5a02bff6e727ab6c22a2719e4f3261a
Shivali Raju Bandi <40134659+ShivaliBandi@users.noreply.github.com>
2021-05-04 12:01:56 +0530

    Create number_mirror.cpp

---
+++ number_mirror.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int ino=0;
+	cin>>ino;
+	cout<<ino;
+	return 0;
+}
+/*
+Number Mirror Problem Code: START01
+
+Problem Statement
+Write a program that accepts a number, n, and outputs the same.
+
+Input
+The only line contains a single integer.
+
+Output
+Output the answer in a single line.
+
+Constraints
+0 ≤ n ≤ 105
+Sample Input
+123
+Sample Output
+123
+
+
+*/
