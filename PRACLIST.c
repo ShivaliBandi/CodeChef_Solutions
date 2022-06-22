22508cbe961e47305242f22acdca6a02fa41b807
Shivali Raju Bandi <40134659+ShivaliBandi@users.noreply.github.com>
2022-06-22 17:09:09 +0530

    Create PRACLIST.c

---
+++ PRACLIST.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int x=0,y=0;
+	scanf("%d%d",&x,&y);
+	printf("%d\n",x-y);
+	return 0;
+}
+
+//refer link for problem statement:https://www.codechef.com/submit/PRACLIST
