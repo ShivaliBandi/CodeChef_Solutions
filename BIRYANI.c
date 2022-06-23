47710980f05197d7817f9ff48753d0cb8adf7109
Shivali Raju Bandi <40134659+ShivaliBandi@users.noreply.github.com>
2022-06-24 00:43:42 +0530

    Create BIRYANI.c

---
+++ BIRYANI.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int testcase=0,a,b;
+	scanf("%d",&testcase);
+	while(testcase--)
+	{
+	    scanf("%d%d",&a,&b);
+	    printf("%d\n",a*b);
+	}
+	return 0;
+}
